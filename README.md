ebf1731e9938f0e16cbc9088323aefeb3da4393f
Deepak Kumar <21131061+kumadee@users.noreply.github.com>
2024-06-05 12:47:00 +0200

    fix: add multi-core support to python script

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ I created this repo to find the prime numbers for a given range of natural numbe
 
 The tests were executed on a 4-cores 64-bit AMD A8-6410 APU (potato CPU :D) and DDR3 16GB RAM (memory is insignificant here).
 
-|range|python3.10|pypy3|rust|Go|
+|range|python3.12|pypy3|rust|Go|
 |-|-|-|-|-|
-|1-1000000|0m5,533s|0m0,953s|0m0,108s|0m0,120s|
-|1-10000000|2m14,175s|0m9,843s|0m2,614s|0m2,584s|
-|1-100000000|Too-long (maybe hours)|4m15,669s|1m12,671s|1m12,408s|
+|1-1000000|0m0,721s|0m0,246s|0m0,108s|0m0,120s|
+|1-10000000|0m19,303s|0m3,712s|0m2,614s|0m2,584s|
+|1-100000000|12m31,370s|2m2,538s|1m12,671s|1m12,408s|
+
--- python/primegen.py
+++ python/primegen.py
@@ -2,6 +2,7 @@
 """Generate prime numbers"""
 
 import math
+import concurrent.futures
 from argparse import ArgumentParser
 
 
@@ -15,9 +16,16 @@ def is_prime(num: int) -> bool:
     return True
 
 
-def count_prime(num: int, show: bool) -> int:
-    count = 1
-    for n in range(3, num, 2):
+def count_prime(start: int, num: int, show: bool) -> int:
+    count = 0
+    if start < 3:
+        count = 1
+        start = 3
+
+    if start % 2 == 0:
+        start += 1
+
+    for n in range(start, num, 2):
         if is_prime(n):
             count += 1
             if show:
@@ -25,26 +33,63 @@ def count_prime(num: int, show: bool) -> int:
     return count
 
 
+def create_chunks(num: int, num_of_chunks: int) -> list[tuple[int, int]]:
+    result = []
+    chunk_size = math.floor(num / num_of_chunks)
+    for i in range(0, num_of_chunks):
+        start = 1 if i == 0 else (i * chunk_size)
+        end = (i + 1) * chunk_size
+        if i + 1 == num_of_chunks:
+            end += num % num_of_chunks
+        result.append((start, end))
+    return result
+
+
+def process_chunks(chunk: tuple[int, int]) -> int:
+    start, end = chunk
+    # print(f"Process chunk ({start}, {end})")
+    return count_prime(start, end, False)
+
+
 if __name__ == "__main__":
     parser = ArgumentParser(description="Python Prime Sieve")
     parser.add_argument(
-        "--limit", "-l", help="Upper limit for calculating prime numbers", type=int, default=1_000_000)
+        "--limit",
+        "-l",
+        help="Upper limit for calculating prime numbers",
+        type=int,
+        default=1_000_000,
+    )
     parser.add_argument(
-        "--show", "-s", help="Print found prime numbers", action="store_true")
+        "--max-workers",
+        "-w",
+        help="Max worker threads for calculating prime numbers",
+        type=int,
+        default=4,
+    )
+    parser.add_argument(
+        "--show", "-s", help="Print found prime numbers", action="store_true"
+    )
     args = parser.parse_args()
     limit = args.limit
 
     prime_counts = {
-        10: 4,                 # Historical data for validating our results - the number of primes
-        100: 25,                # to be found under some limit, such as 168 primes under 1000
+        10: 4,  # Historical data for validating our results - the number of primes
+        100: 25,  # to be found under some limit, such as 168 primes under 1000
         1_000: 168,
         10_000: 1229,
         100_000: 9592,
         1_000_000: 78498,
         10_000_000: 664579,
-        100_000_000: 5761455
+        100_000_000: 5761455,
     }
-    c = count_prime(limit, args.show)
-    print(f"Total primes for {limit} natural numbers is {c}.")
-    result = c == prime_counts.get(limit, 0)
+    chunks = create_chunks(args.limit, args.max_workers)
+    total_prime_count = 0
+    with concurrent.futures.ProcessPoolExecutor(
+        max_workers=args.max_workers
+    ) as executor:
+        for count in executor.map(process_chunks, chunks):
+            total_prime_count += count
+    print(f"Total primes for {limit} natural numbers is {total_prime_count}.")
+    result = total_prime_count == prime_counts.get(limit, 0)
     print(f"Is number of prime correct? => {result}")
